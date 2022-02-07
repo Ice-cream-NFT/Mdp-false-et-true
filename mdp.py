952acd9545b9d6d3fbbc0f33eea1e9fbc1543095
Ice-cream-NFT <98552378+Ice-cream-NFT@users.noreply.github.com>
2022-02-07 13:34:22 +0100

    Add files via upload

---
+++ mdp.py
@@ -0,0 +1,40 @@
+
+id = input("Quel est l'identifient")
+abc = "personne"
+idj = True
+idf = False
+
+if idf == False :
+	while idf == False :
+		if id ==  abc :
+			idj = print ("id juste :) ")
+			idj = False
+		else :
+			idf = input ("id pas juste :( ")
+			idf = False
+		if idj == False :
+			input ("suivant =====>")
+			idf = True
+		elif idf == True :			
+			nnn = input ("recommançer====>")
+			
+			
+mdp = input("Quel est le mot de passe")
+abc = "time"
+mdpj = True
+mdpf = False
+
+if mdpf == False :
+	while mdpf == False :
+		if mdp ==  abc :
+			mdpj = print ("mdp juste :) ")
+			mdpj = True
+		else :
+			mdpf = input ("mdp pas juste :( ")
+			mdpf = False
+		if mdpj == False :
+			input ("suivant =====>")
+			mdpf =True
+		elif mdpf == True :
+			nn = input("recommançer===>")
+			
